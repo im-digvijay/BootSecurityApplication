e35e13c2dfebcbfd8f1ce15e863868a8ebcbc749
Digvijay Singh <singhdigvijay0007@gmail.com>
2022-04-27 00:43:44 +0530

    initial commit

---
+++ src/main/java/rc/bootsecurity/security/UserPrincipalDetailsService.java
@@ -0,0 +1,25 @@
+package rc.bootsecurity.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import rc.bootsecurity.db.UserRepository;
+import rc.bootsecurity.model.User;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        return new UserPrincipal(user);
+    }
+
+}
